a4fe5fbcfd043fa923aaa4e703acddbbca79a97f
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-10-09 19:17:52 +0545

    Add form and remove route from objects

--- constants.cjs
+++ constants.cjs
@@ -1,9 +1,5 @@
 module.exports = {
   PORT: 3000,
-  ROUTES: {
-    root: "/",
-    newMessage: "/new",
-  },
   messages: [
     { text: "Hi there", user: "Amando", added: new Date() },
     { text: "Hello World!", user: "Charles", added: new Date() },
--- index.cjs
+++ index.cjs
@@ -1,6 +1,6 @@
 const express = require("express");
 const { resolve } = require("node:path");
-const { PORT, ROUTES, messages } = require("./constants.cjs");
+const { PORT, messages } = require("./constants.cjs");
 
 require("dotenv").config();
 
@@ -10,16 +10,18 @@ app.set("views", resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(resolve(__dirname, "public")));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get(ROUTES.root, (req, res) => {
+app.get("/", (req, res) => {
   res.render("index", { messages });
 });
 
-app.get(ROUTES.newMessage, (req, res) => {
+app.get("/new", (req, res) => {
+  console.log(req.body);
   res.render("new");
 });
 
-app.post(ROUTES.newMessage, (req, res) => {
+app.post("/new", (req, res) => {
   res.send(req.body);
 });
 
